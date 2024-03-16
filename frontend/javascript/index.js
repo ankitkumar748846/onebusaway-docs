0bf33a80da71435a53ef666a42a272ce55e9241d
ankitkumar748846 <ankitkumar748846@gmail.com>
2024-03-16 22:39:17 +0530

    change in dark theme

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -16,6 +16,37 @@ import { enableDocSearch } from "./search";
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}"
 
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+//   if (isDarkMode) {
+//     document.documentElement.setAttribute('data-theme', 'dark');
+//   } 
+
+// });
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   const isLightMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+
+//   if (isLightMode) {
+//     document.documentElement.setAttribute('data-theme', 'light');
+//   } 
+
+// });
+
+// window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+//   const newColorScheme = event.matches ? "light" : "dark";
+// });
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    console.log('light mode');
+  document.documentElement.setAttribute('data-theme', 'light');
+  }
+  });
+
 console.info("Bridgetown is loaded!")
 
 document.addEventListener("DOMContentLoaded", function(event) {
